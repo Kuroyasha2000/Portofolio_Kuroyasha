c24748bd6ef218ee440cf30c3ae5b7c3e1a25b33
Kukuh Rizki <132186516+Kuroyasha2000@users.noreply.github.com>
2024-11-13 22:16:06 +0700

    Portofolio_Kuroyasha

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,14 @@
+import styles from './ProjectsStyles.module.css';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <h1>COMING SOON PROJECTS</h1>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
